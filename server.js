91fa4d3f82ddb01b70f0509dc0ec29df1f8fefee
XebiaNetCore <pkonar@xebia.com>
2018-06-17 16:05:33 +0530

    added projects page to views

--- server.js
+++ server.js
@@ -40,6 +40,12 @@ app.get("/bad",(req,res) => {
     });
 });
 
+app.get("/projects",(req,res) => {
+    res.render("projects.hbs",{
+      pageTitle : "Projects"
+    });
+});
+
 app.listen(port,()=>{
     console.log(`The server is up and running on ${port}`);
 });
